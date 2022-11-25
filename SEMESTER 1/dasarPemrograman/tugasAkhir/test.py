ae6e33d4d756f41177f158016638e2740f8cd1dd
Alfie Hz <115875275+alfie-hz@users.noreply.github.com>
2022-11-25 11:28:51 +0700

    Delete test.py

--- SEMESTER 1/dasarPemrograman/tugasAkhir/test.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try:
-        angka = int(input("Masukkan angka : "));
-        if angka == int(angka):
-            print("Input yang anda masukkan benar.");
-            break;
-    except:
-        print("Input yang anda masukkan salah.");
